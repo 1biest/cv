4d074b1a7961e8ef4a08eaf5d4fa37a18123caa8
biest <93846276+1biest@users.noreply.github.com>
2023-10-15 18:33:27 -0600

    new data schema

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import './App.css';
-import ReactDOM from "react-dom";
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Modal from '@mui/material/Modal';
-
-import SectionBody from './components/sectionBody';
-import Navigation from './components/navigation';
-import Section1 from './components/sections/section-1';
-import MobileNav from './components/mobile-nav';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+import "./App.css";
+import SectionBody from "./components/sectionBody";
+import Navigation from "./components/navigation";
+import Section1 from "./components/sections/section-1";
+import MobileNav from "./components/mobile-nav";
 
 function App() {
   return (
     <div className="App">
       <div className="mobileHeader">
-        <div className ="mobileNav">
+        <div className="mobileNav">
           <MobileNav />
         </div>
       </div>
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,63 +1,107 @@
-import * as React from 'react';
-import '../App.css';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Menu from '@mui/material/Menu';
-import Box from '@mui/material/Box';
-import scrollTo from 'gatsby-plugin-smoothscroll';
-import IconButton from '@mui/material/IconButton';
-import { Twitter, LinkedIn, GitHub, ContactPage } from '@mui/icons-material';
+import * as React from "react";
+import "../App.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import scrollTo from "gatsby-plugin-smoothscroll";
+import IconButton from "@mui/material/IconButton";
+import { Twitter, LinkedIn, GitHub, ContactPage } from "@mui/icons-material";
 
-import ProfilePhoto from './profilePhoto';
+import ProfilePhoto from "./profilePhoto";
 
 const buttons = [
-    <Button onClick={() => scrollTo("#sectionPEN")} key="one">CodePen</Button>,
-    <Button onClick={() => scrollTo("#sectionSF")} key="one">stakefish/f2pool</Button>,
-    <Button onClick={() => scrollTo("#sectionAA")} key="two">Artrageous Advertising</Button>,
-    <Button onClick={() => scrollTo("#sectionDAG")} key="three">Davis Automotive Group</Button>,
-    <Button onClick={() => scrollTo("#sectionAG")} key="four">Agriculture</Button>,
-    <Button onClick={() => scrollTo("#sectionED")} key="five">Education</Button>,
+  <Button onClick={() => scrollTo("#sectionPEN")} key="one">
+    CodePen
+  </Button>,
+  <Button onClick={() => scrollTo("#sectionSF")} key="one">
+    stakefish/f2pool
+  </Button>,
+  <Button onClick={() => scrollTo("#sectionAA")} key="two">
+    Artrageous Advertising
+  </Button>,
+  <Button onClick={() => scrollTo("#sectionDAG")} key="three">
+    Davis Automotive Group
+  </Button>,
+  <Button onClick={() => scrollTo("#sectionAG")} key="four">
+    Agriculture
+  </Button>,
+  <Button onClick={() => scrollTo("#sectionED")} key="five">
+    Education
+  </Button>,
 ];
-
 
 export default function ImgMediaCard() {
   return (
-    <> 
-        <div className="navWrapper navWrapperClose">
-            <div style={{cursor: 'pointer'}} onClick={() => scrollTo("#section1")} ><ProfilePhoto /></div>
-            <h2 className="nameTitle">Logan<br></br> Biesterfeldt</h2>
-            <p className="nameSubtitle"></p>
-        
-            <IconButton color="primary">
-                <Twitter onClick={()=> window.open("https://twitter.com/1biesterfeldt", "_blank")}>Twitter</Twitter>
-            </IconButton>
-            <IconButton color="primary">
-                <LinkedIn onClick={()=> window.open("https://www.linkedin.com/in/biest/", "_blank")} />
-            </IconButton>
-            <IconButton color="primary">
-                <GitHub onClick={()=> window.open("https://github.com/1biest", "_blank")} />
-            </IconButton>
-            <div style={{height:"12px"}} />
-            <ButtonGroup
-                orientation="vertical"
-                aria-label="vertical button group"
-                variant="text"
-            >
-                {buttons}
-            </ButtonGroup>
+    <>
+      <div className="navWrapper navWrapperClose">
+        <div
+          style={{ cursor: "pointer" }}
+          onClick={() => scrollTo("#section1")}
+        >
+          <ProfilePhoto />
         </div>
+        <h2 className="nameTitle">
+          Logan<br></br> Biesterfeldt
+        </h2>
+        <p className="nameSubtitle"></p>
 
-        <div className="navFooter navFooterClose">
-            <Button variant="outlined" style={{ fontWeight: '600', marginBottom: '18px'}} startIcon={<ContactPage />} onClick={() => window.open("https://drive.google.com/file/d/1AwS_CczNhqqmsRkWaFxzTYfP1vN1jTeO/view?usp=sharing", "_blank")}>Download CV</Button>
-            <h4>Phone</h4>
-            <p><a href="tel:1-403-795-3929">+1 (403) 795-3929</a></p>
-            <h4>Email</h4>
-            <p><a href='&#109;a&#105;l&#116;o&#58;l&#111;&#103;an&#37;&#50;Eb&#105;est&#101;&#37;72%&#54;6eld%74&#64;gmai&#108;&#46;c%&#54;F%&#54;&#68;'>&#108;ogan&#46;bi&#101;&#115;&#116;&#101;&#114;feldt&#64;gmai&#108;&#46;&#99;o&#109;</a></p>
-            <h4>Location</h4>
-            <p>Calgary, Canada</p>
-            
-        </div>
+        <IconButton color="primary">
+          <Twitter
+            onClick={() =>
+              window.open("https://twitter.com/1biesterfeldt", "_blank")
+            }
+          >
+            Twitter
+          </Twitter>
+        </IconButton>
+        <IconButton color="primary">
+          <LinkedIn
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/biest/", "_blank")
+            }
+          />
+        </IconButton>
+        <IconButton color="primary">
+          <GitHub
+            onClick={() => window.open("https://github.com/1biest", "_blank")}
+          />
+        </IconButton>
+        <div style={{ height: "12px" }} />
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="vertical button group"
+          variant="text"
+        >
+          {buttons}
+        </ButtonGroup>
+      </div>
+
+      <div className="navFooter navFooterClose">
+        <Button
+          variant="outlined"
+          style={{ fontWeight: "600", marginBottom: "18px" }}
+          startIcon={<ContactPage />}
+          onClick={() =>
+            window.open(
+              "https://drive.google.com/file/d/1AwS_CczNhqqmsRkWaFxzTYfP1vN1jTeO/view?usp=sharing",
+              "_blank"
+            )
+          }
+        >
+          Download CV
+        </Button>
+        <h4>Phone</h4>
+        <p>
+          <a href="tel:1-403-795-3929">+1 (403) 795-3929</a>
+        </p>
+        <h4>Email</h4>
+        <p>
+          <a href="&#109;a&#105;l&#116;o&#58;l&#111;&#103;an&#37;&#50;Eb&#105;est&#101;&#37;72%&#54;6eld%74&#64;gmai&#108;&#46;c%&#54;F%&#54;&#68;">
+            &#108;ogan&#46;bi&#101;&#115;&#116;&#101;&#114;feldt&#64;gmai&#108;&#46;&#99;o&#109;
+          </a>
+        </p>
+        <h4>Location</h4>
+        <p>Calgary, Canada</p>
+      </div>
     </>
   );
 }
