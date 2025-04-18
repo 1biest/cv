85709f9adcc03c2dcc2d02f549d5ab2f823b3094
biest <93846276+1biest@users.noreply.github.com>
2025-04-18 17:34:08 -0600

    layout adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import { useState } from 'react';
 import BackgroundFX from './Components/BackgroundFx';
 import GithubStats from './Components/GithubStats';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import { faCaretLeft, faCaretRight, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
 const experience = [
   {
-    title: 'Neptune Webapp',
+    title: 'Neptune Finance',
     content:
       'Most of my experience with modern frontend frameworks comes from leading the development of the <a href="https://app.nept.finance" target="_blank" rel="noopener noreferrer"><strong>Neptune Finance</strong></a> web application — a borrowing and lending platform built with CosmWasm and deployed on the <a href="https://injective.com" target="_blank" rel="noopener noreferrer"><strong>Injective</strong></a> blockchain. My responsibilities have spanned across frontend engineering, UI/UX design, branding, deployments, and overall user experience strategy.',
     content2:
@@ -31,6 +31,7 @@ const experience = [
       'Midjourney',
       'Fleek',
     ],
+    ctas: [['Neptune Finance', 'https://app.nept.finance']],
   },
   {
     title: 'Cryptech',
@@ -85,15 +86,55 @@ export default function Home() {
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-10 px-20 pt-20">
-        <div className="col-span-3 lg:col-span-1 flex flex-col gap-3">
-          <h1 className="text-5xl">Logan Biesterfeldt</h1>
-          <h2 className="text-lg pb-1 font-bold">Web3 Frontend Engineer</h2>
-          <p className="text-[#99ACC7]">
-            I build elegant frontend interfaces for smart contract based decentralized applications.
-          </p>
+      <div className="grid grid-cols-3 gap-10 px-20 pt-20 selection:bg-[#EBCB8E] selection:text-[#101322]">
+        <div className="relative lg:fixed col-span-3 lg:col-span-1 w-full lg:w-[28%] lg:max-w-[600px] flex flex-col gap-6 pr-8 cursor-default">
+          <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-2">
+              <h1 className="text-5xl font-bold">Logan Biesterfeldt</h1>
+              <h2 className="text-lg pb-1 font-bold">Web3 Frontend Engineer</h2>
+            </div>
+            <div className="flex flex-col gap-4 text-[#99ACC7]">
+              <p className="">
+                I build elegant frontend interfaces for smart contract based decentralized
+                applications.
+              </p>
+              <p className="">
+                My GitHub exprience is on the right, but my journey didn&apos;t start there. I have
+                several years of experience in graphic design, motion graphics, and marketing,
+                including my time with{' '}
+                <span className="font-bold">
+                  <a href="https://stake.fish/" target="_blank" rel="noopener noreferrer">
+                    stake.fish
+                  </a>
+                </span>
+              </p>
+            </div>
+            <div className="flex flex-col gap-1 text-xs pt-8">
+              {experience.map((exp, index) => (
+                <span
+                  key={index}
+                  className={`${index === highlightIndex ? 'text-[#EBCB8E]' : ''} cursor-pointer hover:text-[#EBCB8E]`}
+                  onClick={() => setHighlightIndex(index)}
+                >
+                  {exp.title}
+                </span>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="col-span-3 lg:col-span-2 max-w-[1200px] flex flex-col gap-4">
+        <div className="relative lg:fixed bottom-0 left-0 px-0 lg:px-20 pb-10 lg:pb-20 flex gap-6">
+          <div
+            className="relative px-4 w-36 h-11 py-2 group cursor-pointer z-30 text-2xl"
+            onClick={() => setHighlightIndex((prev) => (prev + 1) % githubStatsData.length)}
+          >
+            <div className="bg-[#1F2C40] rounded-lg absolute inset-0 z-0 opacity-20 hover:opacity-50 backdrop-blur-sm border  border-white/10 border-t-white/30 backdrop-blur-md transition duration-300 ease-in-out"></div>
+            <button className="relative flex group-hover:text-[#EBCB8E] transition z-40 pt-[2px] pointer-events-none">
+              <FontAwesomeIcon icon={faFilePdf} />
+              <span className="pl-2 w-20 text-lg">Resume</span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-3 lg:col-span-2 lg:col-start-2 max-w-[1200px] flex flex-col gap-4">
           {/* <h1 className="text-4xl font-bold">Logan Biesterfeldt</h1>
           <h2 className="text-2xl font-semibold text-[#99ACC7] hover:text-[#EAC99C]">
             Software Engineer
@@ -129,10 +170,10 @@ export default function Home() {
                 ))}
               </div>
             </div>
-            <div className="flex flex-col gap-8 pl-[100px]">
+            <div className="flex flex-col gap-8 pl-[100px] cursor-default">
               <div className="max-w-[1000px] relative flex flex-col gap-6">
                 <h4 className="text-3xl font-bold">{experience[highlightIndex].title || ''}</h4>
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col prose prose-invert max-w-none gap-4">
                   <p
                     className="text-[#99ACC7] z-30 relative"
                     dangerouslySetInnerHTML={{ __html: experience[highlightIndex].content || '' }}
@@ -159,7 +200,7 @@ export default function Home() {
                 </div>
                 {/* <div className="bg-[#1F2C40] rounded-lg absolute inset-0 z-0 opacity-0 hover:opacity-50 backdrop-blur-sm border  border-white/10 border-t-white/30 backdrop-blur-md transition duration-300 ease-in-out"></div> */}
               </div>
-              <div className="flex justify-start gap-2 mb-2 pr-1 text-2xl px-4">
+              <div className="flex justify-start gap-2 mb-2 pr-1 text-2xl">
                 <div
                   className="relative px-4 py-1 group cursor-pointer z-30"
                   onClick={() =>
@@ -169,7 +210,7 @@ export default function Home() {
                   }
                 >
                   <div className="bg-[#1F2C40] rounded-lg absolute inset-0 z-0 opacity-20 hover:opacity-50 backdrop-blur-sm border  border-white/10 border-t-white/30 backdrop-blur-md transition duration-300 ease-in-out"></div>
-                  <button className="text-[#99ACC7] group-hover:text-[#EBCB8E] transition z-40 pt-[2px]">
+                  <button className="relative group-hover:text-[#EBCB8E] transition z-40 pt-[2px] pointer-events-none">
                     <FontAwesomeIcon icon={faCaretLeft} />
                   </button>
                 </div>
@@ -178,7 +219,7 @@ export default function Home() {
                   onClick={() => setHighlightIndex((prev) => (prev + 1) % githubStatsData.length)}
                 >
                   <div className="bg-[#1F2C40] rounded-lg absolute inset-0 z-0 opacity-20 hover:opacity-50 backdrop-blur-sm border  border-white/10 border-t-white/30 backdrop-blur-md transition duration-300 ease-in-out"></div>
-                  <button className="text-[#99ACC7] group-hover:text-[#EBCB8E] transition z-40 pt-[2px]">
+                  <button className="relative group-hover:text-[#EBCB8E] transition z-40 pt-[2px] pointer-events-none">
                     <FontAwesomeIcon icon={faCaretRight} />
                   </button>
                 </div>
